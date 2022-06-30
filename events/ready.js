f142b11777db9339e793d05613873476a29a1f2d
Minazeh <conradng07@gmail.com>
2022-06-30 10:58:30 +0800

    text update

--- events/ready.js
+++ events/ready.js
@@ -31,7 +31,7 @@ module.exports = {
         
         warAttendance.sync();
         //Log Bot's username and the amount of servers its in to console
-        console.log(`${bot.user.username} is online on ${bot.guilds.cache.size} servers!!`);
+        console.log(`${bot.user.username} is online on ${bot.guilds.cache.size} servers!!!`);
         
         //Set the Presence of the bot user
         bot.user.setPresence({ activities: [{ name: 'Bobo is currently struggling.'}] });
